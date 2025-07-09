9c6e4006df6ab75c5a954c384f37091e6fc5a252
Danik4ek <137286837+Danik4ek@users.noreply.github.com>
2025-07-09 12:25:03 +0700

    Update StealABrainrot.lua

--- StealABrainrot.lua
+++ StealABrainrot.lua
@@ -1,3 +1,64 @@
-for _, obj in ipairs(workspace:GetDescendants()) do
-    print(obj:GetFullName()) -- Путь к объекту
+local brainrotList = {
+    "Noobini pizzanini",
+    "Lirilì Larilà",
+    "Tim Cheese",
+    "Fluriflura",
+    "Talpa di ferro",
+    "Svinina Bombardino",
+    "Pipi Kiwi",
+    "Trippi Troppi",
+    "Tung Tung Tung Sahur",
+    "Gangster Footera",
+    "Bandito Bobritto",
+    "Boneca Ambalabu",
+    "Ta Ta Ta Ta Sahur",
+    "Tric Trac Baraboom",
+    "Cappuccino Assassino",
+    "Brr Brr Patapim",
+    "Trulimero Trulicina",
+    "Bambini Crostini",
+    "Bananita Dolphinita",
+    "Perochello Lemonchello",
+    "Brri Brri Bicus Dicus Bombicus",
+    "Burbaloni Loliloli",
+    "Chimpanzini Bananini",
+    "Ballerina Cappuccina",
+    "Chef Crabracadabra",
+    "Lionel Cactuseli",
+    "Glorbo Fruttodrillo",
+    "Blueberrinni Octopusini",
+    "Frigo Camelo",
+    "Orangutini Ananassini",
+    "Rhino Toasterino",
+    "Bombardiro Crocodilo",
+    "Bombombini Gusini",
+    "Cavallo Virtuoso",
+    "Cocofanto Elefanto",
+    "Gattatino Nyanino",
+    "Girafa Celestre",
+    "Matteo",
+    "Tralalero Tralala",
+    "Odin Din Din Dun",
+    "Unclito Samito",
+    "Trenostruzzo Turbo 3000",
+    "La Vacca Saturno Saturnita",
+    "Sammyni Spiderini",
+    "Los Tralaleritos",
+    "Graipuss Medussi",
+    "La Grande Combinazione",
+    "Garama and Madundung",
+    "Orcalero Orcala and de Tabrak",
+    "Developini Braziliaspidini",
+    "Kings Coleslaw",
+    "Ballerino Lololo",
+    "Crocodillo Ananasinno"
+}
+
+for _, brainrotName in ipairs(brainrotList) do
+    local found = workspace:FindFirstChild(brainrotName)
+    if found then
+        print(brainrotName .. " — идет")
+    else
+        print(brainrotName .. " — нету на сцене")
+    end
 end
